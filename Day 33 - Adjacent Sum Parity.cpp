de5da45e00ad80af75e791c0a0f4c061aaa124a1
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2024-11-04 18:27:12 +0530

    Add files via upload

---
+++ Day 33 - Adjacent Sum Parity.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,sum=0;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++){
+	    cin>>arr[i];
+	    sum +=arr[i];
+	}
+	if(sum%2==0){
+	    cout<<"yes"<<endl;
+	}
+	else
+	cout<<"no"<<endl;
+	}
+	return 0;
+}
